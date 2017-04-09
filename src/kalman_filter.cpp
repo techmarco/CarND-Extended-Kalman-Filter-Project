dad4727e7f6f3d9c4266aa68375ba4da56f48ad5
Marco P. Nogueira <marco.nogueira@gatech.edu>
2017-04-09 00:29:38 -0400

    Kalman Filter: +state prediction, KF update, EKF update

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,7 +1,9 @@
 #include "kalman_filter.h"
+#include "tools.h"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
+using namespace std;
 
 KalmanFilter::KalmanFilter() {}
 
@@ -15,25 +17,58 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
   H_ = H_in;
   R_ = R_in;
   Q_ = Q_in;
+  I_ = MatrixXd::Identity(4, 4);
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  //predict the state
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  //perform measurement update
+  VectorXd y = z - H_ * x_;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_ * Ht * Si;
+
+  //new state
+  x_ = x_ + (K * y);
+  P_ = (I_ - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  Tools tools;
+
+  float px, py, px2, py2, vx, vy;
+  px = x_[0];
+  py = x_[1];
+  vx = x_[2];
+  vy = x_[3];
+
+  px2 = px * px;
+  py2 = py * py;
+
+  VectorXd h = VectorXd(3);
+  h(0) = sqrt(px2 + py2);
+  h(1) = atan2(py, px);
+  //divide by zero check
+  if (h(0) < 0.00001)
+    h(2) = 0;
+  else
+    h(2) = (px*vx+py*vy) / h(0);
+
+  //perform measurement update
+  VectorXd y = z - h;
+  MatrixXd Hj = tools.CalculateJacobian(x_);
+  MatrixXd Ht = Hj.transpose();
+  MatrixXd S = Hj * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_ * Ht * Si;
+
+  //new state
+  x_ = x_ + (K * y);
+  P_ = (I_ - K * Hj) * P_;
 }
