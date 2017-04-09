247c0753d9b266b7eb90c12783f2486c44140b2e
Marco P. Nogueira <marco.nogueira@gatech.edu>
2017-04-09 00:13:31 -0400

    tools: +RMSE and Jacobian calculation utility functions

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,15 +11,62 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  //validate inputs
+  if ((estimations.size() == 0) || estimations.size() != ground_truth.size())
+  {
+    std::cout << "ERROR - invalid inputs to CalculateRMSE()!";
+    return rmse;
+  }
+
+  //squared residual accumulation
+  for (int i=0; i<estimations.size(); ++i)
+  {
+    VectorXd diff = estimations[i]-ground_truth[i];
+    diff = diff.array() * diff.array();
+    rmse += diff;
+  }
+
+  //calculate mean
+  rmse /= estimations.size();
+
+  //calculate square root
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+  MatrixXd Hj(3, 4);
+
+  Hj << 0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0;
+
+  //state parameters
+  float px, py, vx, vy;
+  px = x_state[0];
+  py = x_state[1];
+  vx = x_state[2];
+  vy = x_state[3];
+
+  //computation for Jacobian matrix
+  float c1, c2, c3;
+
+  c1 = px*px+py*py;
+  //divide by zero check
+  if(fabs(c1) < 0.00001) {
+    return Hj;
+  }
+  c2 = sqrt(c1);
+  c3 = (c1*c2);
+
+  //compute Jacobian
+  Hj <<              (px/c2),             (py/c2),     0,     0,
+                    -(py/c1),             (px/c1),     0,     0,
+         py*(vx*py-vy*px)/c3, px*(vy*px-vx*py)/c3, px/c2, py/c2;
+
+ return Hj;
 }
